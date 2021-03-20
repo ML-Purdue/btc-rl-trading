46a4cedcbfde4ebde53026dd15286680f27f3db4
TrungBui-Purdue-5913 <bui23@purdue.edu>
2021-03-20 10:27:50 -0400

    Change the data from Adj Close to Close

--- trading_bot/utils.py
+++ trading_bot/utils.py
@@ -40,7 +40,7 @@ def get_stock_data(stock_file):
     """Reads stock data from csv file
     """
     df = pd.read_csv(stock_file)
-    return list(df['Close'])
+    return list(df['Adj Close'])
 
 
 def switch_k_backend_device():
