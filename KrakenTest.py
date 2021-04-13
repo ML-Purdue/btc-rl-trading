3803a40afa233e46f04db0b0c3ac16a80e3f03a8
Naren Rachapalli <70213604+nrachapa@users.noreply.github.com>
2021-04-13 17:57:10 -0400

    Update KrakenTest.py

--- KrakenTest.py
+++ KrakenTest.py
@@ -1,12 +1,21 @@
-import KrakenCredentials as cred
+import credentials as cred
 import KrakenAPI as krak
-
 
 def test_private_methods():
   client = krak.KrakenClient(cred.api_key, cred.api_secret)
+  print(client.get_trade_history())
+  print('\n')
+  print(client.get_closed_orders())
+  print('\n')
   print(client.get_ticker('BTC'))
+  print('\n')
   print(client.get_account_balance())
+  print('\n')
   print(client.get_trade_balance())
+  print('\n')
+  #print(client.buy_market_order('USDC', 5))
+  print('\n')
+  #print(client.sell_market_order('USDC', 5))
   return
   
 def main():
@@ -15,4 +24,3 @@ def main():
 
 if __name__ == '__main__':
   main()
-  
