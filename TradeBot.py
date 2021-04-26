26571dc1c9bd87b21ab9b42ee05b0939f0e592e0
jacob-zietek <xoqcpn@gmail.com>
2021-04-25 23:35:20 -0400

    initial commit for tradebot class

---
+++ TradeBot.py
@@ -0,0 +1,14 @@
+from trading_bot.agent import Agent
+from trading_bot.methods import evaluate_model
+from trading_bot.utils import (get_stock_data, format_currency, format_position, show_eval_result, switch_k_backend_device)
+
+class TradeBot():
+
+    def __init__(self, api_key, api_secret, model_name, gui_refresh_fcn):
+        # TODO
+
+    def panick_sell():
+        # TODO
+
+    def main_thread():
+        # TODO
