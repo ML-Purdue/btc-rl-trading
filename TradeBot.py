0400b74a544550c6a364ea61c3e6241fbcf9a845
jacob-zietek <xoqcpn@gmail.com>
2021-04-26 00:08:47 -0400

    Subtle changes in class structure

--- TradeBot.py
+++ TradeBot.py
@@ -1,14 +1,22 @@
 from trading_bot.agent import Agent
 from trading_bot.methods import evaluate_model
 from trading_bot.utils import (get_stock_data, format_currency, format_position, show_eval_result, switch_k_backend_device)
+import threading #https://www.tutorialspoint.com/python/python_multithreading.htm
 
-class TradeBot():
+class TradeBot(threading.Thread):
 
-    def __init__(self, api_key, api_secret, model_name, gui_refresh_fcn):
+    def __init__(self, threadID, name, api_key, api_secret, gui_refresh_fcn):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+
         # TODO
 
     def panick_sell():
         # TODO
 
-    def main_thread():
-        # TODO
+    def run():
+        # TODO 
+
+
+        
